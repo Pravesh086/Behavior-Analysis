23f82cde1022ae07447bc3ee9b656d00aa855308
Purnagya08 <purnagya.raj26nov@gmail.com>
2026-05-03 23:04:27 +0530

    deployment fix

--- server/src/server.js
+++ server/src/server.js
@@ -1,13 +1,13 @@
 import { connectDatabase } from "./config/database.js";
 import { app } from "./app.js";
 
-const port = Number(process.env.PORT) || 5000;
+const PORT = Number(process.env.PORT) || 5000;
 
 const startServer = async () => {
   await connectDatabase(process.env.MONGODB_URI);
 
-  app.listen(port, () => {
-    console.log(`Server listening on http://localhost:${port}`);
+  app.listen(PORT, "0.0.0.0", () => {
+    console.log(`Server running on port ${PORT}`);
   });
 };
 
